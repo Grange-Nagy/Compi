44f8fb887512c9785304225d458a25ecd393cd13
Grange-Nagy <grangenagy@gmail.com>
2020-06-14 09:01:37 -0400

    more instructions supported

--- compiASM/compiASM.cpp
+++ compiASM/compiASM.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
+#include <sstream>
 #include "json.hpp"
 #include "parseASM.h"
 
@@ -17,7 +19,7 @@ int main(int argc, char **argv)
 	readJSON.close();
 	
 	
-	std::string asmLine[4] = {"xor","eax","eax",""};
+	std::string asmLine[4] = {"shrd","rax","r13","0x1a"};
 	std::string * asmLineParsed = new std::string[4];
 	parseLine(x86data, asmLine, asmLineParsed);
 	
@@ -33,16 +35,65 @@ int main(int argc, char **argv)
 	std::cout << "\n" << "\n";
 	
 	 for (int i = 0; i < 4; i++){
-		std::cout << asmLineParsed[i] << " "; 
+		//std::cout << asmLineParsed[i] << " "; 
 	} 
 	
-	std::cout << "\n" << "\n";
+	//std::cout << "\n" << "\n";
 	///////////////////////////////////////////////
 	
 	
 	
 	
-	searchForInstruction(x86data, asmLineParsed, asmLine);
+	std::vector <std::string> instructionVector = searchForInstruction(x86data, asmLineParsed, asmLine);
+	std::string opcodeString = instructionVector[3];
+	int opcodesCount = (std::count(opcodeString.begin(), opcodeString.end(), ' '))+1;
+	std::string opcodeArray[opcodesCount];
+	int indexStream = 0;
+	std::stringstream opcodeStringStream(opcodeString);
+	while (opcodeStringStream.good() && indexStream < opcodesCount){
+		opcodeStringStream >> opcodeArray[indexStream];
+		indexStream++;
+	}
+	
+	
+	for (int printIndex = 0; printIndex < opcodesCount; printIndex++){
+		//std::cout << opcodeArray[printIndex];
+	}
+	//std::cout << "\n";
+	
+	if (opcodeArray[0] == "REX.W"){
+		std::stringstream REXBitConvStream;
+		REXBitConvStream << std::hex << std::uppercase << createREXW(x86data, instructionVector, asmLine).to_ulong();
+		opcodeArray[0] = REXBitConvStream.str();
+	}
+	
+	
+	for (int indexCheckI = 0; indexCheckI < opcodesCount; indexCheckI++){
+		for (int indexCheckIeach = 0; indexCheckIeach < opcodesCount; indexCheckIeach++){
+			if (opcodeArray[indexCheckIeach] == asmLineParsed[indexCheckI] ){
+				opcodeArray[indexCheckIeach] = asmLine[indexCheckI];
+			}
+	}
+	}
+	
+	
+	
+	for (int indexCheckRM = 0; indexCheckRM < opcodesCount; indexCheckRM++){
+		if (opcodeArray[indexCheckRM] == "/r" || opcodeArray[indexCheckRM] == "/0" ){
+			std::stringstream modRMBitConvStream;
+			modRMBitConvStream << std::hex << std::uppercase << createModRM(x86data, instructionVector, asmLine).to_ulong();
+			opcodeArray[indexCheckRM] = modRMBitConvStream.str();
+		}
+	}
+	
+	
+	
+	
+	std::cout << "MACHINE CODE: ";
+	for (int printIndex = 0; printIndex < opcodesCount; printIndex++){
+		std::cout << opcodeArray[printIndex] << " ";
+	}
+	std::cout << "\n";
 	delete[] asmLineParsed; //this should maybe be built into the search or something
 	
 	
--- compiASM/parseASM.h
+++ compiASM/parseASM.h
@@ -2,10 +2,29 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
 #include "json.hpp"
 
 
 using json = nlohmann::json;
+
+//returns the binary representation of the register
+std::bitset<4> getRegisterEncode(json x86data, const std::string registerString){
+	//std::cout << "regstring: " << registerString << "\n";
+	for (const auto& encodeDataLine : x86data["registerEncode"]){
+			for (int j = 1; j < 12; j++){
+				if (encodeDataLine[j] == registerString){
+					std::bitset<4> returnBitset((std::string)encodeDataLine[0]);
+					return returnBitset;
+				}
+			}
+	}
+	std::cout << "FATAL ERROR: COULD NOT FIND REGISTER ENCODE\n";
+	return NULL;
+}
+
+
+
 
 
 std::string getRegisterClass(json x86data, const std::string registerString){
@@ -98,7 +117,7 @@ void parseLine(json x86data, const std::string * asmLine, std::string * asmLineP
 }
 
 
-std::string * searchForInstruction(json x86data, const std::string * asmLineParsed, const std::string * asmLine){
+std::vector <std::string> searchForInstruction(json x86data, const std::string * asmLineParsed, const std::string * asmLine){
 	
 	for (const auto& instructionDataLine : x86data["instructions"]) {
 		//std::string noPermString = instructionDataLine[1];
@@ -130,40 +149,137 @@ std::string * searchForInstruction(json x86data, const std::string * asmLinePars
 			
 			
 			if (currentInstructionStringArray[0].find(asmLineParsed[1]) != std::string::npos 
-			&& currentInstructionStringArray[1].find(asmLineParsed[2]) != std::string::npos 
-			&& currentInstructionStringArray[2].find(asmLineParsed[3]) != std::string::npos){
-				std::cout << "FOUND!" << "\n";
-				std::string returnLINE[5];
+			&&  currentInstructionStringArray[1].find(asmLineParsed[2]) != std::string::npos 
+			&&  currentInstructionStringArray[2].find(asmLineParsed[3]) != std::string::npos){
+				//std::cout << "FOUND!" << "\n";
+				std::vector <std::string> returnLINE;
 				for (int i = 0; i < 5; i++){
-					returnLINE[i] = instructionDataLine[i];
-					std::cout << "instructionLINEdata: " << returnLINE[i] << "\n";
+					returnLINE.push_back(instructionDataLine[i]);
+					//std::cout << "instructionLINEdata: " << returnLINE[i] << "\n";
 				}
 				
 				return returnLINE;
 			}
 		}
 	}
-	return NULL;
+	std::vector <std::string> NULLreturn = {""};
+	return NULLreturn;
 }
 
 
-/*std::string * getOperandArray(const std::string * asmLineParsed, const std::string * asmLine){
-	
-	
-	std::string operandStringArray[sizeof(asmline)-1];
-	
-	for (int i = 1; i < 4; i++){
-		if (asmLineParsed[1].front() == 'r'){
-			operandString.append(asmLineParsed[1]);
-			operandString.append("/m");
-			operandString.append(asmLineParsed[1].begin()+1, asmLineParsed[1].end());
-			operandString.append("");
+
+std::bitset<8> createREXW(json x86data, std::vector <std::string> instructionVector, const std::string * asmLine){
+	std::bitset<8> rexwBitset("01001000");
+	int operandCount = 0;
+	for (int i = 1; i < 3; i++){
+		if (asmLine[i] != ""){
+			operandCount++;
+		}
+	}
+	int indexR = -1;
+	for (int j = 0; j < operandCount; j++){
+		if (instructionVector[2][j] == 'R'){
+			//std::cout << "found R @ " << j << "\n";
+			indexR = j;
+			break;
 		}
 	}
 	
 	
-	operandString.append(asmLineParsed[2]);
 	
-	std::cout << operandString<< "\n";
+	if (indexR == -1){
+		//std::cout << "R NOT FOUND IN REXW INSTRUCTION\n";
+	}else{
+		std::bitset<4> encodeBitset = getRegisterEncode(x86data, asmLine[indexR+1]);
+		//std::cout << encodeBitset << "\n";
+		rexwBitset[2] = encodeBitset[3];
+		//std::cout << rexwBitset << "\n";
+	}
 	
-}*/
+	
+	int indexM = -1;
+	for (int k = 0; k < operandCount; k++){
+		if (instructionVector[2][k] == 'M'){
+			//std::cout << "found M @ " << k << "\n";
+			indexM = k;
+			break;
+		}
+	}
+	if (indexM == -1){
+		//std::cout << "M NOT FOUND IN REXW INSTRUCTION\n";
+	}else{
+		std::bitset<4> encodeBitset2 = getRegisterEncode(x86data, asmLine[indexM+1]);
+		//std::cout << encodeBitset2 << "\n";
+		rexwBitset[0] = encodeBitset2[3];
+		//std::cout << rexwBitset << "\n";
+	}
+	
+	
+	
+	//std::cout << "REX.W is: 0x" << std::hex << std::uppercase << rexwBitset.to_ulong();
+	//std::cout << operandCount << "\n";
+	
+	
+	return rexwBitset;
+}
+
+std::bitset<8> createModRM(json x86data, std::vector <std::string> instructionVector, const std::string * asmLine){
+	std::bitset<8> modRMBitset("11000000");
+	int operandCount = 0;
+	for (int i = 1; i < 3; i++){
+		if (asmLine[i] != ""){
+			operandCount++;
+		}
+	}
+	int indexM = -1;
+	for (int j = 0; j < operandCount; j++){
+		if (instructionVector[2][j] == 'M'){
+			//std::cout << "found M @ " << j << "\n";
+			indexM = j;
+			break;
+		}
+	}
+	
+	
+	
+	if (indexM == -1){
+		//std::cout << "M NOT FOUND IN modRM INSTRUCTION\n";
+	}else{
+		std::bitset<4> encodeBitset = getRegisterEncode(x86data, asmLine[indexM+1]);
+		//std::cout << encodeBitset << "\n";
+		modRMBitset[2] = encodeBitset[2];
+		modRMBitset[1] = encodeBitset[1];
+		modRMBitset[0] = encodeBitset[0];
+		//std::cout << modRMBitset << "\n";
+	}
+	
+	
+	int indexR = -1;
+	for (int k = 0; k < operandCount; k++){
+		if (instructionVector[2][k] == 'R'){
+			//std::cout << "found M @ " << k << "\n";
+			indexR = k;
+			break;
+		}
+	}
+	if (indexR == -1){
+		//std::cout << "M NOT FOUND IN modRM INSTRUCTION\n";
+	}else{
+		std::bitset<4> encodeBitset2 = getRegisterEncode(x86data, asmLine[indexR+1]);
+		//std::cout << encodeBitset2 << "\n";
+		modRMBitset[5] = encodeBitset2[2];
+		modRMBitset[4] = encodeBitset2[1];
+		modRMBitset[3] = encodeBitset2[0];
+		//std::cout << modRMBitset << "\n";
+	}
+	
+	
+	
+	//std::cout << "modRM is: 0x" << std::hex << std::uppercase << modRMBitset.to_ulong();
+	//std::cout << operandCount << "\n";
+	
+	
+	return modRMBitset;
+}
+
+
